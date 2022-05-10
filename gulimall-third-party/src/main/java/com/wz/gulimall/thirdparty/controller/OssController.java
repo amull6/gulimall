07758235ca127cad54beb18acbf834c18c5983f7
qinjie <250676978@qq.com>
2022-05-10 16:19:07 +0800

    ali云文件上传-获取服务端签名

---
+++ gulimall-third-party/src/main/java/com/wz/gulimall/thirdparty/controller/OssController.java
@@ -0,0 +1,73 @@
+package com.wz.gulimall.thirdparty.controller;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @Author: 2014015130
+ * @Date: 2022/5/10 15:28
+ * @Description:
+ */
+@RestController
+public class OssController {
+
+    @Value ("${spring.cloud.alicloud.oss.endpoint}")
+    String endpoint ;
+
+    @Value("${spring.cloud.alicloud.oss.bucket}")
+    String bucket ;
+
+    @Value("${spring.cloud.alicloud.access-key}")
+    String accessId ;
+    @Value("${spring.cloud.alicloud.secret-key}")
+    String accessKey ;
+    @Autowired
+    OSS ossClient;
+
+    @RequestMapping("/oss/policy")
+    public Map<String, String> policy(){
+        String host = "https://" + bucket + "." + endpoint;
+        String dir = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        Map<String, String> respMap = null;
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+            // respMap.put("expire", formatISO8601Date(expiration));
+        } catch (Exception e) {
+            // Assert.fail(e.getMessage());
+            System.out.println(e.getMessage());
+        }finally {
+            ossClient.shutdown();
+        }
+        return respMap;
+    }
+}
