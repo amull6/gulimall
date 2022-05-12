42c5b3e833f1f0e6982533adf59a5b4388a4c85d
qinjie <250676978@qq.com>
2022-05-12 13:47:34 +0800

    属性分组列表

--- gulimall-product/src/main/java/com/wz/gulimall/product/service/impl/AttrGroupServiceImpl.java
+++ gulimall-product/src/main/java/com/wz/gulimall/product/service/impl/AttrGroupServiceImpl.java
@@ -1,7 +1,10 @@
 package com.wz.gulimall.product.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,6 +14,7 @@ import com.wz.common.utils.Query;
 import com.wz.gulimall.product.dao.AttrGroupDao;
 import com.wz.gulimall.product.entity.AttrGroupEntity;
 import com.wz.gulimall.product.service.AttrGroupService;
+import org.springframework.util.StringUtils;
 
 
 @Service("attrGroupService")
@@ -26,4 +30,28 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         return new PageUtils(page);
     }
 
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, long catId) {
+        String key = (String) params.get("key");
+        QueryWrapper<AttrGroupEntity> queryWrapper = new QueryWrapper<AttrGroupEntity>();
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.and((i) -> {
+                i.eq("attr_group_id", key).or().like("attr_group_name", key);
+            });
+        }
+        IPage<AttrGroupEntity> page = new Page<>();
+        if (catId == 0) {
+            page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params),
+                    queryWrapper
+            );
+        } else {
+            queryWrapper.eq("catelog_id",catId);
+            page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params),
+                    queryWrapper
+            );
+        }
+        return new PageUtils(page);
+    }
 }
