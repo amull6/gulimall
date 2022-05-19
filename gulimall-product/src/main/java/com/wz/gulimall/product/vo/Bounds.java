f0bcfcf17e6f380f32ee4370c378714152c060f9
qinjie <250676978@qq.com>
2022-05-19 21:58:42 +0800

    保存SPU SKU

--- gulimall-product/src/main/java/com/wz/gulimall/product/vo/Bounds.java
+++ gulimall-product/src/main/java/com/wz/gulimall/product/vo/Bounds.java
@@ -3,28 +3,20 @@
   */
 package com.wz.gulimall.product.vo;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
 /**
  * Auto-generated: 2022-05-18 15:44:50
  *
  * @author bejson.com (i@bejson.com)
  * @website http://www.bejson.com/java2pojo/
  */
+@Data
 public class Bounds {
 
-    private int buyBounds;
-    private int growBounds;
-    public void setBuyBounds(int buyBounds) {
-         this.buyBounds = buyBounds;
-     }
-     public int getBuyBounds() {
-         return buyBounds;
-     }
-
-    public void setGrowBounds(int growBounds) {
-         this.growBounds = growBounds;
-     }
-     public int getGrowBounds() {
-         return growBounds;
-     }
+    private BigDecimal buyBounds;
+    private BigDecimal growBounds;
 
 }
