8da395ca7ef88195b3706dff64468e2a087935d4
qinjie <250676978@qq.com>
2022-05-26 20:55:12 +0800

    elasticsearch检索

---
+++ gulimall-search/src/main/java/com/wz/gulimall/search/GuimallSearchApplication.java
@@ -0,0 +1,13 @@
+package com.wz.gulimall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuimallSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuimallSearchApplication.class, args);
+    }
+
+}
---
+++ gulimall-search/src/test/java/com/wz/gulimall/search/GuimallSearchApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wz.gulimall.search;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuimallSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
