8e5ea59ef9850a5c6f3c1e3e7eafb70e4523118c
qinjie <250676978@qq.com>
2022-05-27 10:12:53 +0800

    Elasticsearchc测试

--- gulimall-search/src/test/java/com/wz/gulimall/search/GuimallSearchApplicationTests.java
+++ gulimall-search/src/test/java/com/wz/gulimall/search/GuimallSearchApplicationTests.java
@@ -6,8 +6,20 @@ import com.wz.gulimall.config.GuliEsConfig;
 import lombok.Data;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.Aggregations;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.Avg;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +27,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,21 +41,106 @@ public class GuimallSearchApplicationTests {
 
     @Test
     public void testIndex() throws IOException {
-        IndexRequest indexRequest = new IndexRequest();
+        // 设置索引
+        IndexRequest indexRequest = new IndexRequest("users");
         indexRequest.id("1");
-        User user = new User();
-        user.setAge(18);
-        user.setUsername("qinjie");
-        user.setGender("男");
-        String js = JSON.toJSONString(user);
-        indexRequest.source(js, XContentType.JSON);
-        IndexResponse index  = restHighLevelClient.index(indexRequest, GuliEsConfig.COMMON_OPTIONS);
-        System.out.println(index);
 
+        User user = new User();
+        user.setUsername("张三");
+        user.setAge(20);
+        user.setGender("男");
+        String jsonString = JSON.toJSONString(user);
+
+        //设置要保存的内容，指定数据和类型
+        indexRequest.source(jsonString, XContentType.JSON);
+
+        //执行创建索引和保存数据
+        IndexResponse index = restHighLevelClient.index(indexRequest, GuliEsConfig.COMMON_OPTIONS);
+
+        System.out.println(index);
+    }
+
+    @Test
+    public void testIndexSearch() throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("bank");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        searchSourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, GuliEsConfig.COMMON_OPTIONS);
+        System.out.println(searchResponse.toString());
+    }
+
+    @Test
+    public void testAggregation() throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("bank");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
+        TermsAggregationBuilder aggregation1 = AggregationBuilders.terms("aggAge")
+                .field("age");
+        AvgAggregationBuilder aggregation2 = AggregationBuilders.avg("agg02").field("balance");
+        aggregation1.subAggregation(aggregation2);
+        AvgAggregationBuilder aggregation3 = AggregationBuilders.avg("agg03").field("balance");
+        searchSourceBuilder.aggregation(aggregation1);
+        searchSourceBuilder.aggregation(aggregation3);
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, GuliEsConfig.COMMON_OPTIONS);
+
+        System.out.println(searchResponse.toString());
+
+        SearchHit[] searchHits = searchResponse.getHits().getHits();
+        for (SearchHit searchHit : searchHits) {
+            String source = searchHit.getSourceAsString();
+            Account account = JSON.parseObject(source, Account.class);
+            System.out.println(account);
+        }
+        Aggregations aggregations = searchResponse.getAggregations();
+        Terms byCompanyAggregation = aggregations.get("aggAge");
+        for( Terms.Bucket bucket:byCompanyAggregation.getBuckets()){
+            String key = bucket.getKeyAsString();
+            System.out.println(key);
+        }
+        Avg averageAge = aggregations.get("agg03");
+        System.out.println(averageAge.getValue());
     }
 
     @Data
-    class User{
+    public static class Account {
+        private int account_number;
+        private int balance;
+        private String firstname;
+        private String lastname;
+        private int age;
+        private String gender;
+        private String address;
+        private String employer;
+        private String email;
+        private String city;
+        private String state;
+
+        @Override
+        public String toString() {
+            return "Account{" +
+                    "account_number=" + account_number +
+                    ", balance=" + balance +
+                    ", firstname='" + firstname + '\'' +
+                    ", lastname='" + lastname + '\'' +
+                    ", age=" + age +
+                    ", gender='" + gender + '\'' +
+                    ", address='" + address + '\'' +
+                    ", employer='" + employer + '\'' +
+                    ", email='" + email + '\'' +
+                    ", city='" + city + '\'' +
+                    ", state='" + state + '\'' +
+                    '}';
+        }
+    }
+
+
+    @Data
+    class User {
         private String username;
         private String gender;
         private Integer age;
