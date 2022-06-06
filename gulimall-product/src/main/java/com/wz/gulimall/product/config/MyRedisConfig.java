3e904d587e2dfd3a9294db6bbaf2cccc2d60dc28
qinjie <250676978@qq.com>
2022-06-06 14:16:13 +0800

    redission锁测试

--- gulimall-product/src/main/java/com/wz/gulimall/product/config/MyRedisConfig.java
+++ gulimall-product/src/main/java/com/wz/gulimall/product/config/MyRedisConfig.java
@@ -3,10 +3,12 @@ package com.wz.gulimall.product.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyRedisConfig {
+    @Bean
     public RedissonClient redisson() {
         RedissonClient redisson = Redisson.create();
         Config config = new Config();
--- gulimall-product/src/main/java/com/wz/gulimall/product/web/IndexController.java
+++ gulimall-product/src/main/java/com/wz/gulimall/product/web/IndexController.java
@@ -3,6 +3,8 @@ package com.wz.gulimall.product.web;
 import com.wz.gulimall.product.entity.CategoryEntity;
 import com.wz.gulimall.product.service.CategoryService;
 import com.wz.gulimall.product.vo.Catalog2Vo;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +19,9 @@ public class IndexController {
     @Autowired
     CategoryService categoryService;
 
+    @Autowired
+    RedissonClient redisson;
+
     @RequestMapping({"/", "/index.html"})
     public String indexPage(Model model) {
         List<CategoryEntity> categories = categoryService.getLevel1Categories();
@@ -26,9 +31,26 @@ public class IndexController {
 
     @RequestMapping("/index/json/catalog.json")
     @ResponseBody
-    public Map<String,List<Catalog2Vo>> catalogJson(Model model) {
+    public Map<String, List<Catalog2Vo>> catalogJson(Model model) {
         Map<String, List<Catalog2Vo>> map = categoryService.getlevel2Categories();
         return map;
     }
 
+
+    @RequestMapping("/index/hello")
+    @ResponseBody
+    public void hello() {
+        RLock rLock = redisson.getLock("my-lock");
+        rLock.lock();
+        try {
+            System.out.println("加锁" + Thread.currentThread().getId());
+            Thread.sleep(30000);
+        } catch (Exception e) {
+
+        } finally {
+            // 如果这里宕机：有看门狗，不用担心
+            System.out.println("解锁" + Thread.currentThread().getId());
+            rLock.unlock();
+        }
+    }
 }
