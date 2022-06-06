062441bbe79f473de962b026725f2b3b2887f3d0
qinjie <250676978@qq.com>
2022-06-06 14:07:02 +0800

    redission配置

---
+++ gulimall-product/src/main/java/com/wz/gulimall/product/config/MyRedisConfig.java
@@ -0,0 +1,16 @@
+package com.wz.gulimall.product.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyRedisConfig {
+    public RedissonClient redisson() {
+        RedissonClient redisson = Redisson.create();
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://124.221.206.12:6379");
+        return Redisson.create(config);
+    }
+}
