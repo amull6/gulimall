6c7deeaaffa15cd8f0c2db80476cac4ae472ea07
qinjie <250676978@qq.com>
2022-06-17 11:34:40 +0800

    商品详情页面Vo构建

---
+++ gulimall-product/src/main/java/com/wz/gulimall/product/vo/SkuItemVo.java
@@ -0,0 +1,42 @@
+package com.wz.gulimall.product.vo;
+
+import com.wz.gulimall.product.entity.SkuImagesEntity;
+import com.wz.gulimall.product.entity.SkuInfoEntity;
+import com.wz.gulimall.product.entity.SpuInfoDescEntity;
+import lombok.Data;
+
+import javax.validation.constraints.FutureOrPresent;
+import java.util.List;
+
+@Data
+class SkuItemVo {
+    //1.sku基本信息  pms_sku_info
+    SkuInfoEntity skuInfoEntity;
+    //2.sku图片信息  pms_sku_images
+    List<SkuImagesEntity> images;
+    //3sku的销售组合信息  pms_sku_sale_attr_value
+    List<SkuItemSaleAttrVo> saleAttr;
+    //4.spu介绍 pms_spu_info_desc
+    SpuInfoDescEntity desp;
+    //5.获取spu规格参数信息 pms_attr_group pms_product_attr_value
+    List<SpuItemGroupAttrVo> groupAttr;
+
+    @Data
+    private static class SkuItemSaleAttrVo {
+        private Long attrId;
+        private String attrName;
+        private List<String> attrValues;
+    }
+
+    @Data
+    private static class SpuItemGroupAttrVo {
+        private String goupName;
+        private List<SpuBaseAttrVo> spuAttrVoList;
+    }
+
+    @Data
+    private static class SpuBaseAttrVo {
+        private String attrName;
+        private String attrValue;
+    }
+}
