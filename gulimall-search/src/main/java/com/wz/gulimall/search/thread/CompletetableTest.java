76539f0cdb4f627168c46aaa669fb4a5e0a3ce91
qinjie <250676978@qq.com>
2022-06-18 10:54:35 +0800

    异步编排测试

--- gulimall-search/src/main/java/com/wz/gulimall/search/thread/CompletetableTest.java
+++ gulimall-search/src/main/java/com/wz/gulimall/search/thread/CompletetableTest.java
@@ -1,9 +1,6 @@
 package com.wz.gulimall.search.thread;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class CompletetableTest {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -27,14 +24,17 @@ public class CompletetableTest {
 //        });
 //        感知并修改
         CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> {
-            System.out.println("线程：" + Thread.currentThread().getId());
+            System.out.println("线程1：" + Thread.currentThread().getId());
             int a = 10 / 5;
             System.out.println(a);
             return a;
-        }, executorService).whenComplete((res, exp) -> {
+        }, executorService).thenApplyAsync((res) -> {
+            System.out.println("线程2：" + Thread.currentThread().getId());
+            return res*2;
+        }).whenComplete((res, exp) -> {
             System.out.println("我的结果" + res);
             System.out.println("我的异常" + exp);
-        }).handle((res, exp)->{
+        }).handle((res, exp) -> {
             if (res != null) {
                 return res * 2;
             }
@@ -45,8 +45,18 @@ public class CompletetableTest {
         }).exceptionally((exp) -> {
             return 10;
         });
+        Future<Void> future01 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("线程3：" + Thread.currentThread().getId());
+            int a = 10 / 5;
+            System.out.println(a);
+            return a;
+        }, executorService).thenRunAsync(() -> {
+            System.out.println("线程4：" + Thread.currentThread().getId());
+        }).whenCompleteAsync((res,exp)->{
+            System.out.println(res);
+            System.out.println(exp);
+        });
         System.out.println("主线程获取结果：" + future.get());
         System.out.println("结束执行main方法");
-
     }
 }
