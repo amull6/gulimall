803ef9a33f77f246e578bebd091b73bb306b47e0
qinjie <250676978@qq.com>
2022-06-26 09:54:21 +0800

    阿里云发送短信

---
+++ gulimall-third-party/src/main/java/com/wz/gulimall/thirdparty/component/SmsComponent.java
@@ -0,0 +1,64 @@
+package com.wz.gulimall.thirdparty.component;
+
+import com.aliyun.tea.*;
+import com.aliyun.dysmsapi20170525.models.*;
+import com.aliyun.teaopenapi.models.*;
+import com.aliyun.teautil.models.*;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+// This file is auto-generated, don't edit it. Thanks.
+@ConfigurationProperties(prefix = "spring.cloud.alicloud.sms")
+@Component
+@Data
+public class SmsComponent {
+
+    private String accessKeyId;
+    private String accessKeySecret;
+    private String endpoint;
+    private String signName;
+    private String templateCode;
+
+    /**
+     * 使用AK&SK初始化账号Client
+     * @param accessKeyId
+     * @param accessKeySecret
+     * @return Client
+     * @throws Exception
+     */
+    public  com.aliyun.dysmsapi20170525.Client createClient(String accessKeyId, String accessKeySecret) throws Exception {
+        Config config = new Config()
+                // 您的 AccessKey ID
+                .setAccessKeyId(accessKeyId)
+                // 您的 AccessKey Secret
+                .setAccessKeySecret(accessKeySecret);
+        // 访问的域名
+        config.endpoint = endpoint;
+        return new com.aliyun.dysmsapi20170525.Client(config);
+    }
+
+    public void sendSmsCode(String phone,String code) throws Exception {
+        String codeJson = "{\"code\":\""+code+"\"}";
+        com.aliyun.dysmsapi20170525.Client client = this.createClient(accessKeyId, accessKeySecret);
+        SendSmsRequest sendSmsRequest = new SendSmsRequest()
+                .setSignName(signName)
+                .setTemplateCode(templateCode)
+                .setPhoneNumbers(phone)
+                .setTemplateParam(codeJson);
+        RuntimeOptions runtime = new RuntimeOptions();
+        try {
+            // 复制代码运行请自行打印 API 的返回值
+            SendSmsResponse sendSmsResponse = client.sendSmsWithOptions(sendSmsRequest, runtime);
+            SendSmsResponseBody sendSmsResponseBody = sendSmsResponse.getBody();
+            System.out.println(sendSmsResponseBody.getMessage());
+        } catch (TeaException error) {
+            // 如有需要，请打印 error
+            com.aliyun.teautil.Common.assertAsString(error.message);
+        } catch (Exception _error) {
+            TeaException error = new TeaException(_error.getMessage(), _error);
+            // 如有需要，请打印 error
+            com.aliyun.teautil.Common.assertAsString(error.message);
+        }
+    }
+}
--- gulimall-third-party/src/test/java/com/wz/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/wz/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,13 +1,26 @@
 package com.wz.gulimall.thirdparty;
 
+import com.wz.gulimall.thirdparty.component.SmsComponent;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class GulimallThirdPartyApplicationTests {
+    @Autowired
+    SmsComponent smsComponent;
+
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void testSendSms() throws Exception {
+        smsComponent.sendSmsCode("15762352663","7781");
     }
 
 }
