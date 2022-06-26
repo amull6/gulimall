88a0b77d62e051b5173f067fffebf2eeb06732a0
qinjie <250676978@qq.com>
2022-06-26 08:56:00 +0800

    认证服务加入总模块管理

---
+++ gulimall-third-party/src/main/java/com/wz/gulimall/thirdparty/SmsComponent.java
@@ -0,0 +1,49 @@
+package com.wz.gulimall.thirdparty;
+
+import org.springframework.stereotype.Component;
+
+// This file is auto-generated, don't edit it. Thanks.
+
+@Component
+public class SmsComponent {
+
+    /**
+     * 使用AK&SK初始化账号Client
+     * @param accessKeyId
+     * @param accessKeySecret
+     * @return Client
+     * @throws Exception
+     */
+    public static com.aliyun.dysmsapi20170525.Client createClient(String accessKeyId, String accessKeySecret) throws Exception {
+        Config config = new Config()
+                // 您的 AccessKey ID
+                .setAccessKeyId(accessKeyId)
+                // 您的 AccessKey Secret
+                .setAccessKeySecret(accessKeySecret);
+        // 访问的域名
+        config.endpoint = "dysmsapi.aliyuncs.com";
+        return new com.aliyun.dysmsapi20170525.Client(config);
+    }
+
+    public void sendSmsCode() {
+        java.util.List<String> args = java.util.Arrays.asList(args_);
+        com.aliyun.dysmsapi20170525.Client client = Sample.createClient("accessKeyId", "accessKeySecret");
+        SendSmsRequest sendSmsRequest = new SendSmsRequest()
+                .setSignName("阿里云短信测试")
+                .setTemplateCode("SMS_154950909")
+                .setPhoneNumbers("15762352663")
+                .setTemplateParam("{\"code\":\"1234\"}");
+        RuntimeOptions runtime = new RuntimeOptions();
+        try {
+            // 复制代码运行请自行打印 API 的返回值
+            client.sendSmsWithOptions(sendSmsRequest, runtime);
+        } catch (TeaException error) {
+            // 如有需要，请打印 error
+            com.aliyun.teautil.Common.assertAsString(error.message);
+        } catch (Exception _error) {
+            TeaException error = new TeaException(_error.getMessage(), _error);
+            // 如有需要，请打印 error
+            com.aliyun.teautil.Common.assertAsString(error.message);
+        }
+    }
+}
