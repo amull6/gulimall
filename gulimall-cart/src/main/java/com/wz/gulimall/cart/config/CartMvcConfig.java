1fbc86e0a73f1e08dac72d08f0e1ae3aa145989f
qinjie <250676978@qq.com>
2022-07-15 13:19:14 +0800

    购物车服务-开启springSession，JSON序列化采用jackson

--- gulimall-cart/src/main/java/com/wz/gulimall/cart/config/CartMvcConfig.java
+++ gulimall-cart/src/main/java/com/wz/gulimall/cart/config/CartMvcConfig.java
@@ -1,9 +1,11 @@
 package com.wz.gulimall.cart.config;
 
 import com.wz.gulimall.cart.interceptor.CartInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CartMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
