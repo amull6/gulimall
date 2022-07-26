8d618107a2aef14e5c700154dcb65611d7a9a91e
qinjie <250676978@qq.com>
2022-07-26 08:41:13 +0800

    订单服务-feign添加拦截器，将页面访问的cookie传入到feign调用创建的请求request

---
+++ gulimall-order/src/main/java/com/wz/gulimall/order/config/GuliFeignConfig.java
@@ -0,0 +1,23 @@
+package com.wz.gulimall.order.config;
+
+import feign.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import javax.servlet.http.HttpServletRequest;
+
+@Configuration
+public class GuliFeignConfig {
+    @Bean("requestInterceptor")
+    public RequestInterceptor requestInterceptor() {
+        RequestInterceptor requestInterceptor = requestTemplate -> {
+            ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            HttpServletRequest httpServletRequest = servletRequestAttributes.getRequest();
+            if (httpServletRequest != null) {
+                requestTemplate.header("cookie", httpServletRequest.getHeader("cookie"));
+            }
+        };
+        return requestInterceptor;
+    }
+}
