5516f4e4981d4c0e58cd18ec1dee3ec2d7248c57
qinjie <250676978@qq.com>
2022-08-05 15:20:46 +0800

    死信队列-延时队列测试

---
+++ gulimall-order/src/main/java/com/wz/gulimall/order/config/MyMQConfig.java
@@ -0,0 +1,59 @@
+package com.wz.gulimall.order.config;
+
+import com.rabbitmq.client.Channel;
+import com.wz.gulimall.order.entity.OrderEntity;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+public class MyMQConfig {
+
+    @RabbitListener(queues = {"order.release.order.queue"})
+    public void receiveOrder(OrderEntity orderEntity, Channel channel, Message message) throws IOException {
+        System.out.println("接收到订单"+message.getBody().toString()+orderEntity.getOrderSn());
+        channel.basicAck(message.getMessageProperties().getDeliveryTag(),false);
+    }
+
+    @Bean
+    public Queue orderDelayQueue() {
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("x-message-ttl", 6000);
+        arguments.put("x-dead-letter-exchange", "order.event.exchange");
+        arguments.put("x-dead-letter-routing-key", "order.release.order");
+//        String name, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
+        return new Queue("order.delay.queue", true, false, false, arguments);
+    }
+
+    @Bean
+    public Queue orderRelaseOrderQueue() {
+//        String name, boolean durable, boolean exclusive, boolean autoDelete, Map<String, Object> arguments
+        return new Queue("order.release.order.queue", true, false, false);
+    }
+
+    @Bean
+    public Exchange orderEventExchange() {
+//        String name, boolean durable, boolean autoDelete, Map<String, Object> arguments
+        return new TopicExchange("order.event.exchange", true, false);
+    }
+
+    @Bean
+    public Binding orderCreateOrderBingding() {
+//        String destination, DestinationType destinationType, String exchange, String routingKey, Map<String, Object> arguments
+        return new Binding("order.delay.queue", Binding.DestinationType.QUEUE, "order.event.exchange", "order.create.order", null);
+    }
+
+    @Bean
+    public Binding orderReleaseOrderBingding() {
+//        String destination, DestinationType destinationType, String exchange, String routingKey, Map<String, Object> arguments
+        return new Binding("order.release.order.queue", Binding.DestinationType.QUEUE, "order.event.exchange", "order.release.order", null);
+    }
+
+
+}
--- gulimall-order/src/main/java/com/wz/gulimall/order/web/OrderWebController.java
+++ gulimall-order/src/main/java/com/wz/gulimall/order/web/OrderWebController.java
@@ -1,23 +1,42 @@
 package com.wz.gulimall.order.web;
 
+import com.wz.gulimall.order.entity.OrderEntity;
 import com.wz.gulimall.order.exception.NoStockException;
 import com.wz.gulimall.order.service.OrderService;
 import com.wz.gulimall.order.vo.OrderConfirmVo;
 import com.wz.gulimall.order.vo.OrderSubmitVo;
 import com.wz.gulimall.order.vo.SubmitOrderResponseVo;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.annotation.AccessType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 @Controller
 public class OrderWebController {
     @Autowired
     OrderService orderService;
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    @RequestMapping("/test/createOrder")
+    @ResponseBody
+    public String createOrder() {
+        OrderEntity orderEntity = new OrderEntity();
+        orderEntity.setOrderSn(UUID.randomUUID().toString());
+//        String var1, String var2, Object var3, MessagePostProcessor var4, CorrelationData var5
+        rabbitTemplate.convertAndSend("order.event.exchange","order.create.order",orderEntity);
+        return "ok";
+    }
 
     @RequestMapping("/submitOrder")
     public String submitOrder(OrderSubmitVo orderSubmitVo, Model model, RedirectAttributes redirectAttributes) {
