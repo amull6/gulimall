94a9ae957fc1d257f688fd8962091616de65ec37
qinjie <250676978@qq.com>
2022-08-07 08:54:26 +0800

    seata-maven引入

--- gulimall-order/src/main/java/com/wz/gulimall/order/config/MySeataConfig.java
+++ gulimall-order/src/main/java/com/wz/gulimall/order/config/MySeataConfig.java
@@ -1,10 +1,9 @@
 package com.wz.gulimall.order.config;
 
 import com.zaxxer.hikari.HikariDataSource;
-import io.seata.rm.datasource.DataSourceProxy;
+//import io.seata.rm.datasource.DataSourceProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 
@@ -14,14 +13,11 @@ import javax.sql.DataSource;
 public class MySeataConfig {
     @Autowired
     DataSourceProperties dataSourceProperties;
-
-    @Bean
-    public DataSource dataSourceProxy(){
-        HikariDataSource dataSource = dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
-        if (StringUtils.hasText(dataSourceProperties.getName())) {
-            dataSource.setPoolName(dataSourceProperties.getName());
-        }
-        return new DataSourceProxy(dataSource);
-    }
-
+//    public DataSource datasource(){
+//        HikariDataSource dataSource = dataSourceProperties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+//        if (StringUtils.hasText(dataSourceProperties.getName())) {
+//            dataSource.setPoolName(dataSourceProperties.getName());
+//        }
+//        return new DataSourceProxy(dataSource);
+//    }
 }
