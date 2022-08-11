6ffb835e1c69356adfdbd5b05d85ae746290343e
qinjie <250676978@qq.com>
2022-08-11 16:18:51 +0800

    商品秒杀-秒杀模块配置

---
+++ gulimall-seckill/src/test/java/com/wz/gulimall/seckill/GulimallSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wz.gulimall.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallSeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
